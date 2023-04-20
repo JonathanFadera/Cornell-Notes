deb004143c9ed5b307b0f3c3857612e4132e3d0c
AlbusSeverus <ericsonhnc@gmail.com>
2023-04-20 13:03:17 -0700

    serverjs file commited

---
+++ server.js
@@ -0,0 +1,16 @@
+const htmlRoutes = require("./routes/htmlRoutes");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+require("./routes/apiRoute");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}!`);
+});
