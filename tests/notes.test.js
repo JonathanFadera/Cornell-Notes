b0a86a3e83db281cf4ef18b235ad74865c14428a
AlbusSeverus <ericsonhnc@gmail.com>
2023-04-20 15:40:05 -0700

    no idea

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -2,18 +2,58 @@ const fs = require("fs");
 const {
   createNewNote,
   validateNote,
-} = require("../lib/notes.js");
-const { notes } = require("../db/db.json");
+  deleteNote,
+} = require("../lib/notes");
+const {
+  notes
+} = require("../db/db.json");
+const {
+  test,
+  expect
+} = require("@jest/globals");
+const {
+  describe
+} = require("yargs");
+const {
+  it
+} = require("mocha");
+
 
 jest.mock('fs');
 
 test("creates a note object", () => {
-    const note = createNewNote(
-      { title: "Test", text: "Test", id: "1234" },
-      notes
-    );
-  
-    expect(note.name).toBe("Test");
-    expect(note.text).toBe("Test");
-    expect(note.id).toBe("1234");
-  });
+  const note = createNewNote({
+      name: "Test",
+      text: "Test",
+      id: "1234"
+    },
+    notes
+  );
+  expect(note).toMatchSnapshot();
+});
+
+test("validates a note object", () => {
+  const note = {
+    name: "Test",
+    text: "Test",
+    id: "1234"
+  };
+  const invalidNote = {
+    name: "Test",
+    id: "1234"
+  };
+  const result = validateNote(note);
+  const result2 = validateNote(invalidNote);
+  expect(result).toBe(true);
+  expect(result2).toBe(false);
+});
+
+test("deletes a note", () => {
+  const note = {
+    name: "Test",
+    text: "Test",
+    id: "1234"
+  };
+  const result = deleteNote(note, notes);
+  expect(result).toBe(true);
+});
