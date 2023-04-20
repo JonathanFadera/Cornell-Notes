e34d98e7773bfb8a3a9fe33159364db43f511a2e
AlbusSeverus <ericsonhnc@gmail.com>
2023-04-20 15:15:56 -0700

    files commited

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,33 +1,48 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
-// Route to read the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-  fs.readFile(__dirname + '/db.json', (err, data) => {
-    if (err) throw err;
-    let notes = JSON.parse(data);
-    res.json(notes);
-  });
-});
+var noteContents;
+module.exports = function (app) {
 
-// Route to receive a new note to save on the request body, add it to the db.json file, and return the new note to the client
-app.post('/api/notes', (req, res) => {
-  let newNote = req.body;
-  newNote.id = uuidv4();
-  fs.readFile(__dirname + '/db.json', (err, data) => {
-    if (err) throw err;
-    let notes = JSON.parse(data);
-    notes.push(newNote);
-    fs.writeFile(__dirname + '/db.json', JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.json(newNote);
+    // Populate the  notes from the JSON file
+    app.get("/api/notes", function (req, res) {
+        readFileAsync("db/db.json", "utf8").then(function (data) {
+            noteContents = JSON.parse(data)
+            res.json(noteContents);
+        })
+    })
+
+    // Posts new notes to the JSON file when entered and saved
+    // Had trouble setting the new Id number - this seems to work correctly on heroku -but not on my localhost
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+        let lastId = 0;
+        if (noteContents.length !== 0) {
+            lastId = noteContents[noteContents.length - 1]["id"];
+        }
+        let newId = lastId + 1;
+        newNote["id"] = newId;
+        noteContents.push(newNote);
+        writeFileAsync("db/db.json", JSON.stringify(noteContents)).then(function () {
+            console.log("Note has been updated");
+        });
+        res.json(newNote);
     });
-  });
-});
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+    // Function to delete notes when trash can is clicked
+    app.delete("/api/notes/:id", function (req, res) {
+        let chosenId = parseInt(req.params.id);
+        for (let i = 0; i < noteContents.length; i++) {
+            if (chosenId === noteContents[i].id) {
+                noteContents.splice(i, 1);
+                let noteJSON = JSON.stringify(noteContents, null, 2);
+                writeFileAsync("db/db.json", noteJSON).then(function () {
+                    console.log("Note has been deleted");
+                });
+            }
+        }
+        res.json(noteContents);
+    });
+};
