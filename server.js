17a32a41c5287eb14d0e5f0a62d4def269c02c5e
AlbusSeverus <ericsonhnc@gmail.com>
2023-04-20 17:42:42 -0700

    file formatted

--- server.js
+++ server.js
@@ -6,73 +6,65 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // DELET request
-
 app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
-        if(err) throw err;
-        const notes = JSON.parse(data);
-        const filteredNotes = notes.filter(note => note.id!== req.params.id);
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(filteredNotes), (err) => 
-        err? console.error(err) : console.log("Successfully deleted note."));
-        res.json(filteredNotes);
-    });
+  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const filteredNotes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(filteredNotes), (err) =>
+      err ? console.error(err) : console.log("Successfully deleted note."));
+    res.json(filteredNotes);
+  });
 });
-
 
 
 // GET and POST request
-
 app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
-        if(err) throw err;
-        const notes = JSON.parse(data);
-        res.json(notes);
-    });
+  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
 });
 
 app.post('/api/notes', (req, res) => {
-    const {title, text} = req.body;
-    if (!title || !text ) {
-        throw new Error("title and text cannot be blank");
-    }
- const newNote = {title, text, id: uuidv4()};
- fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
-    if(err) {
-        console.log(err);
+  const { title, text } = req.body;
+  if (!title || !text) {
+    throw new Error("title and text cannot be blank");
+  }
+  const newNote = { title, text, id: uuidv4() };
+  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+    if (err) {
+      console.log(err);
     }
     else {
-        const dbNotes = JSON.parse(data);
-        dbNotes.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(dbNotes),(err) => 
+      const dbNotes = JSON.parse(data);
+      dbNotes.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(dbNotes), (err) =>
         err ? console.error(err) : console.log("Successfully added note."))
     }
 
- })
- const response = {
+  })
+  const response = {
     body: newNote
- }
- console.log(response)
- res.json(response)
+  }
+  console.log(response)
+  res.json(response)
 })
-
-
 
 // HTML routes for notes.html and index.html
 
-app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname,"/public/notes.html"));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
-
 
 app.listen(PORT, () =>
   console.log(`Listening for requests on port ${PORT}! 🏎️`)
