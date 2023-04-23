6e8aa5bffa02b81930fc7df172baf89619ac64a0
AlbusSeverus <ericsonhnc@gmail.com>
2023-04-22 20:22:57 -0700

    jericho

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -2,38 +2,38 @@
 const fs = require("fs");
 const path = require("path");
 const notes = require("../db/db.json");
-// This is the function we want to test
+//  function we want to test
 const { createNewNote, validateNote } = require("../lib/notes");
-// This is the test suite
+//  test suite
 describe("notes", () => {
-  // This is the test for the createNewNote() function
+  //  test for the createNewNote() function
   describe("createNewNote", () => {
-    // This is the test for creating a note object
+    //  test for creating a note object
     it("should create a new note object", () => {
-      // This is an example of a mock data
+      // example of a mock data
       const note = createNewNote(
         { title: "Test Title", text: "Test text" },
         notes
       );
-      // This is an example of an expected value
+      // example of an expected value
       const expectedNote = {
         title: "Test Title",
         text: "Test text",
         id: expect.any(String),
       };
-      // This is an example of a Jest expectation
+      // example of a Jest expectation
       expect(note).toEqual(expectedNote);
     });
-    // This is the test for when data is invalid
+    //  test for when data is invalid
     it("should return an error when title or text is not entered", () => {
       const cb = jest.fn();
       const note = createNewNote({ title: "Test Title" }, notes);
       expect(cb).toBeCalled();
     });
   });
-  // This is the test for the validateNote() function
+  //  test for the validateNote() function
   describe("validateNote", () => {
-    // This is the test for when data is valid
+    //  test for when data is valid
     it("should return false when title or text is not entered", () => {
       const invalidNote = {
         title: "Test Title",
